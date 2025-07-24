6c7c656b801e919b76dce77314f97d901aea3d55
Cereso Odisi Rodrigues <cereso.rodrigues@anfitriaoprime.com.br>
2025-07-24 15:59:06 -0300

    feat: binary search

---
+++ src/binary-tree/binary-tree.test.ts
@@ -0,0 +1,54 @@
+import { expect, test } from "vitest";
+import { BinaryTree, BinaryTreeNode } from "./binary-tree";
+
+const binaryTree = new BinaryTree<number>();
+
+binaryTree.insert(5);
+binaryTree.insert(3);
+binaryTree.insert(8);
+binaryTree.insert(1);
+binaryTree.insert(4);
+binaryTree.insert(7);
+binaryTree.insert(9);
+
+test("Should traverse binary tree in pre-order", () => {
+  const expected: number[] = [5, 3, 1, 4, 8, 7, 9];
+
+  const rs = binaryTree.traversePreOrder();
+
+  expect(rs).toEqual(expected);
+});
+
+test("Should traverse binary tree in in-order", () => {
+  const expected: number[] = [1, 3, 4, 5, 7, 8, 9];
+
+  const rs = binaryTree.traverseInOrder();
+
+  expect(rs).toEqual(expected);
+});
+
+test("Should traverse binary tree in post-order", () => {
+  const expected: number[] = [1, 4, 3, 7, 9, 8, 5];
+
+  const rs = binaryTree.traversePostOrder();
+
+  expect(rs).toEqual(expected);
+});
+
+test("Should find item in binary tree", () => {
+  const element = 9;
+
+  const expected: BinaryTreeNode<number> = new BinaryTreeNode(element);
+
+  const rs = binaryTree.find(element);
+
+  expect(rs).toEqual(expected);
+});
+
+test("Should not find item in binary tree", () => {
+  const element = 2;
+
+  const rs = binaryTree.find(element);
+
+  expect(rs).toBeNull();
+});
---
+++ src/binary-tree/binary-tree.ts
@@ -0,0 +1,117 @@
+export class BinaryTreeNode<T> {
+  left: BinaryTreeNode<T> | null = null;
+  right: BinaryTreeNode<T> | null = null;
+  value: T;
+
+  constructor(value: T) {
+    this.value = value;
+  }
+}
+
+export class BinaryTree<T> {
+  private root: BinaryTreeNode<T> | null = null;
+
+  find(value: T): BinaryTreeNode<T> | null {
+    return this.findNode(this.root, value);
+  }
+
+  insert(value: T): void {
+    if (this.root === null) {
+      this.root = new BinaryTreeNode(value);
+    } else {
+      this.insertNode(this.root, value);
+    }
+  }
+
+  traverseInOrder(): Array<T> {
+    const rs = new Array<T>();
+
+    this.inOrder(rs, this.root);
+
+    return rs;
+  }
+
+  traversePostOrder(): Array<T> {
+    const rs = new Array<T>();
+
+    this.postOrder(rs, this.root);
+
+    return rs;
+  }
+
+  traversePreOrder(): Array<T> {
+    const rs = new Array<T>();
+
+    this.preOrder(rs, this.root);
+
+    return rs;
+  }
+
+  private findNode(node: BinaryTreeNode<T> | null, value: T): BinaryTreeNode<T> | null {
+    if (node == null) {
+      return null;
+    }
+
+    if (value === node.value) {
+      return node;
+    }
+
+    if (value < node.value) {
+      return this.findNode(node.left, value);
+    }
+
+    return this.findNode(node.right, value);
+  }
+
+  private inOrder(rs: Array<T>, node: BinaryTreeNode<T> | null): void {
+    if (node === null) {
+      return;
+    }
+
+    this.inOrder(rs, node.left);
+
+    rs.push(node.value);
+
+    this.inOrder(rs, node.right);
+  }
+
+  private insertNode(node: BinaryTreeNode<T>, value: T): void {
+    if (value < node.value) {
+      if (node.left === null) {
+        node.left = new BinaryTreeNode(value);
+      } else {
+        this.insertNode(node.left, value);
+      }
+    } else if (value > node.value) {
+      if (node.right === null) {
+        node.right = new BinaryTreeNode(value);
+      } else {
+        this.insertNode(node.right, value);
+      }
+    }
+  }
+
+  private postOrder(rs: Array<T>, node: BinaryTreeNode<T> | null): void {
+    if (node === null) {
+      return;
+    }
+
+    this.postOrder(rs, node.left);
+
+    this.postOrder(rs, node.right);
+
+    rs.push(node.value);
+  }
+
+  private preOrder(rs: Array<T>, node: BinaryTreeNode<T> | null): void {
+    if (node === null) {
+      return;
+    }
+
+    rs.push(node.value);
+
+    this.preOrder(rs, node.left);
+
+    this.preOrder(rs, node.right);
+  }
+}
